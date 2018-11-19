3c2f1fc0e068dc02b4b55f5cada76467fb978a14
harvus1 <38709992+harvus1@users.noreply.github.com>
2018-11-18 20:56:23 -0600

    Update Ultimo Proyecto.rb

--- Ultimo Proyecto.rb
+++ Ultimo Proyecto.rb
@@ -1,162 +1,194 @@
-def limpiar
+def limpiar_pantalla
     system ('clear')
 end
 
- def ver (cola)
-    aux = cola[:frente]
-    auxb = cola[:frente]
-    for x in (1..(cola.size-1))
-        for y in (1..(cola.size-1))
-            if auxb[:valor] > aux[:valor]
-            asd = aux[:valor]
-            aux[:valor] = auxb[:valor]
-            auxb[:valor] = asd
-            end
-         auxb=auxb[:siguiente]
-        end
-        aux= aux[:siguiente]
-        auxb = cola[:frente]
-    end
-    aux = cola[:frente]
-    while aux != nil
-        puts aux[:valor]
-        aux = aux[:siguiente] 
-    end  
-end
- def verl (lista)
-    aux = lista[:frente]
-    auxb = lista[:frente]
-    for x in (1..(lista.size-1))
-        for y in (1..(lista.size-1))
-            if auxb[:valor] > aux[:valor]
-            asd = aux[:valor]
-            aux[:valor] = auxb[:valor]
-            auxb[:valor] = asd
-            end
-         auxb=auxb[:siguiente]
-        end
-        aux= aux[:siguiente]
-        auxb = lista[:frente]
-    end
-    aux = lista[:frente]
-    while aux != nil
-        puts aux[:valor]
-        aux = aux[:siguiente] 
-    end  
-end
- def eliminar(cola)
-aux = cola[:frente]
-cola[:frente] = nil
-cola[:frente] = aux[:siguiente]
-end
- def eliminarl(lista)
-    aux = lista[:tope]
-    lista[:tope] = nil
-    lista[:tope] = aux[:siguiente]
-end
- def insertar (cola)
-    limpiar()
-     print 'Inserte Un Numero: '
-    n = gets.chomp.to_i
-         elemento = {
-        valor: n,
-        siguiente: nil
+def ordenar_lista(lista, valor)
+
+
+    for x in 0..valor.size-1 
+        a=valor[x]
+        elemento={
+            valor: a,
+            siguiente: nil,
         }
-    if cola[:esta_vacia] == true
-        cola[:tope] = elemento
-        cola[:frente] = elemento
-        cola[:esta_vacia] = false
-    else 
-        aux = cola[:tope] 
-        aux[:siguiente] = elemento 
-        elemento[:siguiente] = nil
-        cola[:tope] = elemento
+    
+
+    if lista[:vacia]==true
+    lista[:tope]=elemento
+    lista[:fondo]=elemento
+    lista[:vacia]=false
+    lista[:tamaño]+=1
+    
+    else
+
+    if a<lista[:tope][:valor]
+    auxt=lista[:tope]
+    elemento[:siguiente]=auxt
+    lista[:tope]=elemento
+    lista[:tamaño]+=1
+    
+    elsif a>lista[:fondo][:valor]
+    auxf=lista[:fondo]
+    auxf[:siguiente]=elemento
+    lista[:fondo]=elemento
+    lista[:tamaño]+=1
+  
+    else
+
+    medios=lista[:tope]
+    lista[:tamaño]=lista[:tamaño]+1
+    antes=nil
+    
+    bole = false
+    while bole == false
+    if a<medios[:valor]
+    elemento[:siguiente]=medios
+    antes[:siguiente]=elemento
+    bole = true
     end
+    antes=medios
+    medios=medios[:siguiente]
+    end
+
+
+end    
 end
- def insertarl (lista)
-    limpiar()
-     print 'Inserte Un Numero: '
-    n = gets.chomp.to_i
-         elemento = {
-        valor: n,
-        siguiente: nil
+end
+end
+
+def ordenarc(cola, valor)
+
+
+    for x in 0..valor.size-1 
+        a=valor[x]
+        elemento={
+            valor: a,
+            siguiente: nil,
         }
-     if lista[:esta_vacia] == true
-        lista[:tope] = elemento
-        lista[:frente] = elemento
-        lista[:esta_vacia] = false
-    else 
-        aux = lista[:tope] 
-        aux[:siguiente] = elemento 
-        elemento[:siguiente] = nil
-        lista[:tope] = elemento
+    
+
+    if cola[:vacia]==true
+    cola[:tope]=elemento
+    cola[:fondo]=elemento
+    cola[:vacia]=false
+    cola[:tamaño]+=1
+    
+    else
+
+    if a<cola[:tope][:valor]
+    auxt=cola[:tope]
+    elemento[:siguiente]=auxt
+    cola[:tope]=elemento
+    cola[:tamaño]+=1
+    
+    elsif a>cola[:fondo][:valor]
+    auxf=cola[:fondo]
+    auxf[:siguiente]=elemento
+    cola[:fondo]=elemento
+    cola[:tamaño]+=1
+  
+    else
+
+    medios=cola[:tope]
+    cola[:tamaño]=cola[:tamaño]+1
+    antes=nil
+    
+    bole = false
+    while bole == false
+    if a<medios[:valor]
+    elemento[:siguiente]=medios
+    antes[:siguiente]=elemento
+    bole = true
     end
-end
- cola = {
-    tope: nil,
-    frente: nil,
-    max: -1,
-    esta_vacia: true 
-}
- lista ={
-    tope: nil,
-    frente: nil,
-    max: -1,
-    esta_vacia: true
-}
- pila = {
-    tope: nil,
-    max: -1,
-    esta_vacia: true
-}
- ap =0
-op = 0
- puts "-----------------------------"
-puts "Proyecto 3"
-puts "En que de sea trabajar?"
-puts "1. Colas"
-puts "2. Pilas"
-puts "3. Listas"
-ap = gets.chomp.to_i
- while op != 4
-puts "Ingrese la opcion que desea"
-puts "1. Insertar"
-puts "2. Establecer el orden"
-puts "3. Eliminar un elemento"
-puts "4. Salir"
-op = gets.chomp.to_i
-limpiar()
-if ap ==1
-if op == 1
-insertar(cola)
-end
-if op == 2
-ver(cola)
-end
-if op == 3 
-eliminar(cola)
+    antes=medios
+    medios=medios[:siguiente]
+    end
+
+
+end    
 end
 end
- if ap ==2
-    if op == 1
-    insertarp(pila)
+end
+
+
+
+limpiar_pantalla()
+opcion = 0
+while (opcion!=4)
+puts 'PROYECTO III'
+puts '------------'
+puts ''
+
+    puts 'METODOS DE ORDENAMIENTO:'
+    puts '1. Colas'
+    puts '2. Pilas'
+    puts '3. Listas'
+    puts '4. Salir'
+    puts ''
+    print 'Ingrese Opcion: '
+    opcion = gets.chomp.to_i
+
+    if opcion == 4
+        limpiar_pantalla()
+        puts '                              ------'
+        puts 'METODO MAS EFICIENTE:        |LISTAS|'
+        puts '                              ------'
+
+    if opcion == 3
+        limpiar_pantalla()
+        lista  = {
+            tope: nil,
+            fondo: nil,
+            vacia: true,
+            tamaño: 0
+        }
+        
+        puts "Ingrese Valores (SEPARADOS POR UN ESPACIO):"
+        aux = gets.chomp
+        valor = aux.split(' ').map(&:to_i)
+        
+        ordenar_lista(lista, valor)
+        
+        as = lista[:tope]
+        while as[:siguiente] != nil
+        puts as[:valor]
+        as = as[:siguiente]
+        end
+        puts as[:valor]
+        gets
+        limpiar_pantalla()
     end
-    if op == 2
-    verp(pila)
+
+    if opcion == 1
+        limpiar_pantalla()
+        cola  = {
+            tope: nil,
+            fondo: nil,
+            vacia: true,
+            tamaño: 0
+        }
+        
+        puts "Ingrese Valores (SEPARADOS POR UN ESPACIO):"
+        aux = gets.chomp
+        valor = aux.split(' ').map(&:to_i)
+        
+        ordenarc(cola, valor)
+        
+        as = cola[:tope]
+        while as[:siguiente] != nil
+        puts as[:valor]
+        as = as[:siguiente]
+        end
+        puts as[:valor]
+        gets
+        limpiar_pantalla()
     end
-    if op == 3 
-    eliminarp(pila)
+
+    if opcion == 2
+
     end
 end
- if ap ==3
-    if op == 1
-    insertarl(lista)
-    end
-    if op == 2
-    verl(lista)
-    end
-    if op == 3 
-    eliminarl(lista)
-    end
 end
- end
+
+
+    
